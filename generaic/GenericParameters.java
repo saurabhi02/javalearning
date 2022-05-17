3e7223e9f1c5cc8de796c4d2be176f1684676a78
Saurabhi Chaubey <chaubeysubhi@gmail.com>
2022-05-17 20:26:41 +0530

    Generic parameters

---
+++ generaic/GenericParameters.java
@@ -0,0 +1,41 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+/*t = types 
+ v = value
+ e = element
+ n=number
+ k=key
+
+*/
+
+class pair<k,v> {
+    private k key;
+    private v val;
+    public pair(k key, v val)
+    {
+        this.key=key;
+        this.val=val;
+    }
+
+    public k getkey()
+    {
+        return key;
+
+    }
+    public v getval()
+    {
+        return val;
+    }
+}
+
+
+
+public class GenericParameters {
+    public static void main(String[] args) {
+        pair<Integer,Integer> p=new pair<>(1,2);
+        System.out.println(p.getkey()+" "+p.getval());
+        
+    }
+        
+}
